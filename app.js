ae9e148d21f04c468e7853c4a59a11189330690d
ridhogenzo <genzocuy123@gmail.com>
2021-09-27 18:24:30 +0700

    templete dashboard admin already complete

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+//router admin
+const adminRouter = require("./routes/admin");
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use(
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+//admin
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
