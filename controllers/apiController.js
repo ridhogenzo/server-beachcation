ff47a1af7aa765821f45b9a4b858d18696fd7301
ridhogenzo <genzocuy123@gmail.com>
2021-10-15 16:36:45 +0700

    testing API with mocha and chai

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,6 +3,8 @@ const Hotels = require("../models/Activity");
 const Traveler = require("../models/Booking");
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const Booking = require("../models/Booking");
+const Member = require("../models/Member");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -102,9 +104,9 @@ module.exports = {
     const {
       idItem,
       duration,
-      price,
-      bookingDateStart,
-      bookingDateEnd,
+      // price,
+      bookingStartDate,
+      bookingEndDate,
       firstName,
       lastName,
       emailAddress,
@@ -112,25 +114,70 @@ module.exports = {
       accountHolder,
       bankFrom,
     } = req.body;
+
     if (!req.file) {
       return res.status(404).json({ message: "Image not found " });
     }
+    console.log(idItem);
     if (
-      idItem === "" ||
-      duration === "" ||
-      price === "" ||
-      bookingDateStart === "" ||
-      bookingDateEnd === "" ||
-      firstName === "" ||
-      lastName === "" ||
-      emailAddress === "" ||
-      phoneNumber === "" ||
-      accountHolder === "" ||
-      bankFrom === ""
+      idItem === undefined ||
+      duration === undefined ||
+      // price === undefined ||
+      bookingStartDate === undefined ||
+      bookingEndDate === undefined ||
+      firstName === undefined ||
+      lastName === undefined ||
+      emailAddress === undefined ||
+      phoneNumber === undefined ||
+      accountHolder === undefined ||
+      bankFrom === undefined
     ) {
       res.status(404).json({ message: "Lengkapi semua field" });
     }
+    const item = await Item.findOne({ _id: idItem });
 
-    res.status(201).json({ message: "Success Booking " });
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    item.sumBooking += 1;
+
+    await item.save();
+
+    let total = item.price * duration;
+    let tax = total * 0.1;
+
+    const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+    const member = await Member.create({
+      firstName,
+      lastName,
+      emailAddress,
+      phoneNumber,
+    });
+
+    const newBooking = {
+      invoice,
+      bookingStartDate,
+      bookingEndDate,
+      total: (total += tax),
+      itemId: {
+        _id: item.id,
+        title: item.title,
+        price: item.price,
+        duration: duration,
+      },
+
+      memberId: member.id,
+      payments: {
+        proofPayment: `images/${req.file.filename}`,
+        bankFrom: bankFrom,
+        accountHolder: accountHolder,
+      },
+    };
+
+    const booking = await Booking.create(newBooking);
+
+    res.status(201).json({ message: "Success Booking", booking });
   },
 };
--- models/Member.js
+++ models/Member.js
@@ -9,7 +9,7 @@ const memberSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  email: {
+  emailAddress: {
     type: String,
     required: true,
   },
