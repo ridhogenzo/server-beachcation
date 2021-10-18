6b02d89e388f83af9446618fa71ba52e70850ce9
ridhogenzo <genzocuy123@gmail.com>
2021-10-18 23:32:08 +0700

    Cors added without dependencies

--- app.js
+++ app.js
@@ -69,4 +69,12 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 module.exports = app;
