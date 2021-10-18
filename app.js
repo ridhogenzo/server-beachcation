72a5bb13a6d8dd682326fb9ccb7513b5106b3fcd
ridhogenzo <genzocuy123@gmail.com>
2021-10-18 23:59:48 +0700

    ganbatte!

--- app.js
+++ app.js
@@ -70,11 +70,6 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 module.exports = app;
