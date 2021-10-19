7d8e63aef6d778e28dd0b42ef842cb6e565e6c8b
ridhogenzo <genzocuy123@gmail.com>
2021-10-19 13:05:21 +0700

    last for the last!

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(
 );
 app.use(flash());
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -69,7 +70,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-app.use(cors());
 
 module.exports = app;
